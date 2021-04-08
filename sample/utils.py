1f663d34955573899eb5b873fe70e62d527e4db5
Ben Griffith <bengriffith@outlook.com>
2021-04-08 17:16:03 -0500

    removed California and Georgia url references

--- sample/utils.py
+++ sample/utils.py
@@ -1,8 +1,6 @@
 # Covid URLs
-california_url = 'https://data.chhs.ca.gov/dataset/f333528b-4d38-4814-bebb-12db1f10f535/resource/046cdd2b-31e5-4d34-9ed3-b48cdbc4be7a/download/covid19cases_test.csv'
 texas_url = 'https://dshs.texas.gov/coronavirus/TexasCOVID19DailyCountyCaseCountData.xlsx'
 ohio_url = 'https://coronavirus.ohio.gov/static/dashboards/COVIDDeathData_CountyOfResidence.csv'
-georgia_url = 'https://opendata.arcgis.com/datasets/d817e06e4b264905a075b9332cd41962_0.geojson'
 cases_url = 'https://static.usafacts.org/public/data/covid-19/covid_confirmed_usafacts.csv?_ga=2.249922758.1108579064.1615472677-188720245.1615472677'
 deaths_url = 'https://static.usafacts.org/public/data/covid-19/covid_deaths_usafacts.csv?_ga=2.247303236.1108579064.1615472677-188720245.1615472677'
 population_url = 'https://static.usafacts.org/public/data/covid-19/covid_county_population_usafacts.csv?_ga=2.246842820.1108579064.1615472677-188720245.1615472677'
