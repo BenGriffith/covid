86b892c339b0c70e1c836356cff983f9a7ab0384
Ben Griffith <bengriffith@outlook.com>
2021-04-28 21:18:15 -0500

    first commit

---
+++ sample/azure-hdinsight-script-actions/packages.sh
@@ -0,0 +1,6 @@
+sudo /usr/bin/anaconda/envs/py36booyah/bin/pip install mysql-connector-python
+sudo /usr/bin/anaconda/envs/py36booyah/bin/pip install azure-storage-blob
+sudo /usr/bin/anaconda/envs/py36booyah/bin/pip install cloudpathlib
+sudo /usr/bin/anaconda/envs/py36booyah/bin/pip install pathlib
+sudo /usr/bin/anaconda/envs/py36booyah/bin/pip install pandas
+sudo /usr/bin/anaconda/envs/py36booyah/bin/pip install openpyxl
