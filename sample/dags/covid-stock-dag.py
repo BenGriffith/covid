5f9929f8024713aa1d396190b89549e0b1742b4d
Ben Griffith <bengriffith@outlook.com>
2021-05-04 17:50:15 -0500

    updated to reference HDInsight specific scripts

--- sample/dags/covid-stock-dag.py
+++ sample/dags/covid-stock-dag.py
@@ -6,7 +6,7 @@ from airflow.utils.dates import days_ago
 
 default_args = {
     'owner': 'airflow',
-    'retries': 2,
+    'retries': 0,
     'retry_delay': timedelta(minutes=1)
 }
 
@@ -14,7 +14,7 @@ dag = DAG(
     'covid-stock-pipeline',
     default_args=default_args,
     description='Covd Case Stock Market Data Pipeline',
-    schedule_interval='0 18 * * 1-5',
+    schedule_interval='@weekly',
     start_date=days_ago(1)
 )
 
@@ -22,18 +22,39 @@ hook_extract = SSHHook(ssh_conn_id='ssh_extract')
 
 task_extract = SSHOperator(
     task_id='extraction',
-    command="python3 driver.py",
+    command='python3 driver.py "initial"',
     ssh_hook=hook_extract,
     dag=dag
 )
 
 hook_clean = SSHHook(ssh_conn_id='ssh_clean')
 
-task_clean = SSHOperator(
-    task_id='clean',
-    command="python3 driver-clean.py",
+task_populate_county = SSHOperator(
+    task_id='populate_county',
+    command='source ~/.bash_profile; /usr/bin/anaconda/envs/py36booyah/bin/python3 driver-population.py "initial"',
     ssh_hook=hook_clean,
     dag=dag
 )
 
-task_extract >> task_clean
+task_clean_states = SSHOperator(
+    task_id='clean_states',
+    command='source ~/.bash_profile; /usr/bin/anaconda/envs/py36booyah/bin/python3 driver-states.py "initial"',
+    ssh_hook=hook_clean,
+    dag=dag
+)
+
+task_clean_usafacts = SSHOperator(
+    task_id='clean_usafacts',
+    command='source ~/.bash_profile; /usr/bin/anaconda/envs/py36booyah/bin/python3 driver-usafacts.py "initial"',
+    ssh_hook=hook_clean,
+    dag=dag
+)
+
+task_clean_financial = SSHOperator(
+    task_id='clean_financial',
+    command='source ~/.bash_profile; /usr/bin/anaconda/envs/py36booyah/bin/python3 driver-financial.py "initial"',
+    ssh_hook=hook_clean,
+    dag=dag
+)
+
+task_extract >> task_populate_county >> task_clean_states >> task_clean_usafacts >> task_clean_financial
